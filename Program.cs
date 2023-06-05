f3c1ccd9988773348d6671533675723c0d93ed60
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-06-06 00:47:29 +0700

    ex

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+//Console.WriteLine("Hello, World!");
+//Ex-Stop Watch
+//Viết chương trình đo thời gian thực thi của thuật toán sắp xếp chọn (selection sort) cho 100,000 số.
+
+using System;
+
+namespace StopWatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tạo đối tượng StopWatch
+            StopWatch stopWatch = new StopWatch();
+
+            //Mảng chứa 100000 số 
+            int[] num = new int[100000];
+            Random rand = new Random();
+
+            //Khởi tạo mảng với các số ngẫu nhiên
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = rand.Next(1, 100000);
+            }
+
+            //Sắp xếp mảng và đo thời gian thực thi
+            stopWatch.Start();
+            SelectionSort(num);
+            stopWatch.Stop();
+
+            //In ra thời gian thực thi
+            Console.WriteLine("Thoi gian thuc thi: " + stopWatch.GetElapsedTime() + "ms");
+        }
+
+        static void SelectionSort(int[] num)
+        {
+            int n= num.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+                for (int j = i+1; j < n; j++)
+                {
+                    if (num[j] < num[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = num[min];
+                num[min] = num[i];
+                num[i] = temp;
+            }
+        }
+    }
+}
+
---
+++ StopWatch.cs
@@ -0,0 +1,37 @@
+﻿namespace StopWatch;
+
+public class StopWatch
+{
+    //Trường startTime và endTime là private với phương thức getter để trả về giá trị của startTime và endTime.
+    public DateTime _startTime;
+    public DateTime _endTime;
+    
+    public DateTime StartTime
+    {
+        get => _startTime;
+    }
+    public DateTime EndTime
+    {
+        get => _endTime;
+    }
+    //Phương thức khởi tạo không tham số khởi tạo startTime với thời gian hiện tại của hệ thống.
+    public StopWatch()
+    {
+        _startTime = DateTime.Now;
+    }
+    //Phương thức Start() để reset startTime về thời gian hiện tại của hệ thống.
+    public void Start()
+    {
+        _startTime = DateTime.Now;
+    }
+    //Phương thức Stop() để thiết đặt endTime về thời gian hiện tại của hệ thống.
+    public void Stop()
+    {
+        _endTime = DateTime.Now;
+    }
+    //Phương thức GetElapsedTime() trả về thời gian đã trôi qua theo số milisecond giây
+    public double GetElapsedTime()
+    {
+        return (_endTime - _startTime).TotalMilliseconds;
+    }
+}
